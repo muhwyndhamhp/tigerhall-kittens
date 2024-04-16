aa779016ba637f5490c10303bd28d7edfbd1bde6
muhwyndham <mwyndham@graveltechnology.com>
2024-04-16 19:12:57 +0700

    create auth middleware
    add auth middleware:
    - if "Authorization" Header not exist, keep continuing but does not set
    the user entity in ctx
    - if invalid, then fail the request
    - if valid, check if real user exists
    - if user exists, add user entity to ctx so resolver can validate if
    user exists or not later

---
+++ pkg/modules/user/middleware.go
@@ -0,0 +1,47 @@
+package user
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/muhwyndhamhp/tigerhall-kittens/pkg/models"
+)
+
+var keyUser = &ctxKey{"user"}
+
+type ctxKey struct {
+	name string
+}
+
+func AuthMiddleware(repo models.UserRepository) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			authHeader := r.Header.Get("Authorization")
+
+			if authHeader == "" {
+				next.ServeHTTP(w, r)
+			}
+
+			tu, err := models.ParseToken(authHeader)
+			if err != nil {
+				http.Error(w, "Invalid token", http.StatusUnauthorized)
+				return
+			}
+
+			u, err := repo.FindByID(tu.ID)
+			if err != nil || u == nil {
+				http.Error(w, "User not found", http.StatusUnauthorized)
+				return
+			}
+
+			ctx := context.WithValue(r.Context(), keyUser, u)
+			r = r.WithContext(ctx)
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func UserByCtx(ctx context.Context) *models.User {
+	return ctx.Value(keyUser).(*models.User)
+}
